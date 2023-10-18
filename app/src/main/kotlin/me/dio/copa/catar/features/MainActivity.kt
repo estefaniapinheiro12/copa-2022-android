f578d88146f36b30e9a1d19deffe32ee602fae3a
estefania <estefaniafreitas21@gmail.com>
2023-10-18 15:33:55 -0300

    N/A

--- app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
+++ app/src/main/kotlin/me/dio/copa/catar/features/MainActivity.kt
@@ -1,6 +1,8 @@
 package me.dio.copa.catar.features
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -30,14 +32,33 @@ class MainActivity : ComponentActivity() {
     private fun observeActions() {
         viewModel.action.observe(this) { action ->
             when (action) {
-                is MainUiAction.MatchesNotFound -> TODO()
-                MainUiAction.Unexpected -> TODO()
+                is MainUiAction.MatchesNotFound -> showErrorDialog("Nenhuma correspondência encontrada.")
+                MainUiAction.Unexpected -> {
+                    logError("Ocorreu um erro inesperado.")
+                    showErrorDialog("Ocorreu um erro inesperado. Tente novamente mais tarde.")
+                }
                 is MainUiAction.DisableNotification ->
                     NotificationMatcherWorker.cancel(applicationContext, action.match)
                 is MainUiAction.EnableNotification ->
                     NotificationMatcherWorker.start(applicationContext, action.match)
             }
         }
+    }
+
+    private fun showErrorDialog(s: String) {
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Erro")
+        builder.setMessage(s)
+        builder.setPositiveButton("OK") { _, _ -> }
+        val dialog = builder.create()
+        dialog.show()
+            
+    }
+
+    private fun logError(errorMessage: String) {
+
+        Log.e("AppError", errorMessage)
     }
 
 
